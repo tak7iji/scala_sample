b66d41db3686e1534735dfc80a37762335af5206
E.T <tak7iji@gmail.com>
2015-06-29 19:27:22 +0900

    add map function

--- option/Option.scala
+++ option/Option.scala
@@ -1,13 +1,13 @@
-package fpinscala
+package errorhandling
 
-sealed trait Option[+A] {
-  val value: A
+//import scala.Nothing
 
+trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
     case None => None
+    case Some(v) => Some(f(v))
   }
 }
 
-case class Some[A](get: A) extends Option[A]
+case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
-
